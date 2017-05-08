502a48ac517f064cd26b01ba3493a5d8afefd0b2
dherrebout <dylanherrebout@gmail.com>
2017-05-08 17:21:28 +0200

    Update

--- folium_map.py
+++
@@ -1,4 +0,0 @@
-import folium
-
-map_osm = folium.Map(location=[52.3, 4.9], zoom_start=12)
-map_osm.save('osm.html')
---
+++ map.py
@@ -0,0 +1,15 @@
+import folium
+
+map_carto = folium.Map(
+    location=[52.3, 4.9],
+    zoom_start=10,
+    tiles='cartodbpositron')
+
+folium.Marker(
+    location=[52.3, 4.9],
+    popup='<h1>Amsterdam</h1><p>Zuid</p>',
+    icon=folium.Icon(color='red')
+    ).add_to(map_carto)
+
+map_carto.save('C:\\Users\\dylan\\OneDrive\\Documenten\\Coding\\Git\\maps'
+               '\\pyprojects_map.py.html')
