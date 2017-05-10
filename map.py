9a388c40941b0d88bf39cc2ad7cee30802b7d394
dherrebout <dylanherrebout@gmail.com>
2017-05-10 19:57:46 +0200

    Update

--- map.py
+++ map.py
@@ -8,14 +8,25 @@ map_carto = folium.Map(
     zoom_start=5,
     tiles='cartodbpositron')
 
+
+def color(elev):
+    if elev in range(0, 1500):
+        color = 'green'
+    elif elev in range(1500, 2500):
+        color = 'brown'
+    elif elev in range(2500, 3000):
+        color = 'orange'
+    else:
+        color = 'red'
+
+    return color
+
+
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
     folium.Marker(
         location=[lat, lon],
         popup=str(name) + ' - ' + str(elev) + 'm',
-        icon=folium.Icon(
-            color='green' if elev < 1000
-            else 'orange' if elev in range(1000, 3000)
-            else 'red')
+        icon=folium.Icon(color=color(elev))
     ).add_to(map_carto)
 
 map_carto.save('C:\\Users\\dylan\\OneDrive\\Documenten\\Coding\\Git\\maps'
