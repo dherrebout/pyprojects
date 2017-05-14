c97baaf2f74eca09cc6f6444ac0f777b889e2805
dherrebout <dylanherrebout@gmail.com>
2017-05-14 22:08:00 +0200

    Update

--- website_blocker.py
+++ website_blocker.py
@@ -6,15 +6,40 @@ redirect = '127.0.0.1'
 # List of websites to be blocked within working hours
 website_list = ['www.facebook.com', 'facebook.com']
 
+# Infinite loop
 while True:
+    # If time is between 9AM and 17PM
     if (dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() <
             dt(dt.now().year, dt.now().month, dt.now().day, 17)):
-        # If time is between 9AM and 17PM
         print('Working hours')
 
+        # Read hosts file
+        with open(hosts_path, 'r') as file:
+            content = file.read()
+            print(content)
+
+            # Check if to be blocked websites are already in hosts
+            for website in website_list:
+                # If already there, do nothing
+                if website in content:
+                    pass
+                # If not there, write blocking lines in hosts file
+                else:
+                    file.write(redirect + '\t' + website + '\n')
+
+    # If time is before 9AM or after 17PM
     else:
-        # If time is before 9AM or after 17PM
         print('Fun hours')
+
+        # Read hosts file
+        with open(hosts_path, 'r') as file:
+            content = file.readlines()
+
+        # Read all lines in hosts
+        for line in content:
+            # If item is not in hosts, append it
+            if not any(website in line for website in website_list):
+                file.write(line)
 
     # End of while loop: sleep for 60 seconds
     time.sleep(60)
